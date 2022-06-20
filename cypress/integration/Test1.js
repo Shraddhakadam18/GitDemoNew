0b3f9c355e907f3dffd912ea845fb82e8aae2800
shraddha <shraddha.kadam1802@gmail.com>
2022-06-20 19:23:23 +0530

    this is commit from Y person on test1.js file

--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -7,5 +7,9 @@ describe('My First Test Suite',function(){
      cy.get('.search-keyword').type('ca')
      cy.get('.product:visible').should('have.length',4)
     })
-    
+
+    it('My Second TestCase',function(){
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.log("second test case")
+       })
 })
